be3db5a1bc0ca783fa1f4bfeb3a6f936209d21af
Rob Peschke <rspesc31@gmail.com>
2020-06-17 09:43:59 -0400

    changed text

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to rob's application." });
 });
 
 // set port, listen for requests
