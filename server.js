93e306e8d5eb9e94390cc62869b99fe82433fae3
Rob Peschke <rspesc31@gmail.com>
2020-06-17 09:37:17 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//parse request of content-type: application/json
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
+});
