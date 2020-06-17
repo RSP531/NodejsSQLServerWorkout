ed0aa6200fcbb6012eb4bb25d45ab212520d41c6
Rob Peschke <rspesc31@gmail.com>
2020-06-17 12:35:21 -0400

    updated db.config"

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,13 @@
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "password",
+//   DB: "WorkoutReactNativeApp"
+// };
+
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "password",
-  DB: "WorkoutReactNativeApp"
+  HOST: "us-cdbr-east-05.cleardb.net",
+  USER: "b83e23ede17feb",
+  PASSWORD: "a1a86447",
+  DB: "heroku_2cb43af8db29622"
 };
